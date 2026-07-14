f4b47e3a9dc5c498b00d80a867d0fbe7a38ab46c
bilalfarooq056 <bilalfarooq878675@gmail.com>
2026-07-14 22:54:46 +0500

    Stage 0: hello server

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+@app.get("/")
+def root():
+    return {"message": "Welcome to the smallest backend!"}
 
 @app.get("/status")
 def get_status():
@@ -9,3 +12,9 @@ def get_status():
 @app.get("/data")
 def get_data():
     return {"id": 1, "info": "This is the smallest possible backend."}
+
+@app.get("/health")
+def get_health():
+    return {"status": "OK"}    
+
+
